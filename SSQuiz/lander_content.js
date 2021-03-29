d7af037963ea9b8f68532e32bd86e2ed4d8526d8
GeoAttr <geoattract@gmail.com>
2021-03-29 18:14:58 +0800

    Update lander_content.js

--- SSQuiz/lander_content.js
+++ SSQuiz/lander_content.js
@@ -1,6 +1,6 @@
 var brand = getURLParameter('brand');
 var prize1 = "$1,000 Sheng Siong Voucher";
-var prize2 = "$500 Sheng Siong Voucher";
+var prize2 = "$1,000 Sheng Siong Grocery Card";
 var serviceName1 = getURLParameter('isp');
 var progressText = "Done";
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
